978ea544b5b597b59f37ce40dff1b40920c90fe9
Widehem Jimmy <jimmywidehem@gmail.com>
2024-06-30 18:43:45 +0200

    Add authentication routes to server.js

---
+++ backend/routes/auth.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+// Ajout d'une ligne vide après les importations
+
+// Inscription
+router.post('/signup', (req, res) => {
+  bcrypt.hash(req.body.password, 10)
+    .then((hash) => {
+      const user = new User({
+        email: req.body.email,
+        password: hash,
+      });
+      user.save()
+        .then(() => res.status(201).json({ message: 'User created!' }))
+        .catch((error) => res.status(400).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }));
+});
+
+// Connexion
+router.post('/login', (req, res) => (
+  User.findOne({ email: req.body.email })
+    .then((user) => {
+      if (!user) {
+        return res.status(401).json({ error: 'User not found!' });
+      }
+      return bcrypt.compare(req.body.password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Incorrect password!' });
+          }
+          const token = jwt.sign(
+            { userId: user.id },
+            'RANDOM_TOKEN_SECRET',
+            { expiresIn: '24h' },
+          );
+          return res.status(200).json({
+            userId: user.id,
+            token,
+          });
+        })
+        .catch((error) => res.status(500).json({ error }));
+    })
+    .catch((error) => res.status(500).json({ error }))
+));
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,17 @@
+require('dotenv').config();
+
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-require('dotenv').config();
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+// eslint-disable-next-line no-console
+console.log('MONGO_URI from .env:', process.env.MONGO_URI);
 
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
@@ -17,6 +22,8 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
     // eslint-disable-next-line no-console
     console.log(err);
   });
+
+app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
